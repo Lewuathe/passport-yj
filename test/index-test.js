95ff43ddc136771370be56c7064d1d9af8391545
Lewuathe <lewuathe@me.com>
2013-02-16 11:08:51 +0900

    added test codes

---
+++ test/index-test.js
@@ -0,0 +1,14 @@
+var vow = require('vows');
+var assert = require('assert');
+var util = require('util');
+var twitter = require('passport-yj');
+
+vows.describe('passport-yj').addBatch({
+  
+  'module': {
+    'should report a version': function (x) {
+      assert.isString(twitter.version);
+    },
+  },
+  
+}).export(module);
