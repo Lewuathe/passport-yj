3569a72b68a8edf27c82bcc76994120eca1e60bc
Marco Azimonti <marco.azimonti.dev@gmail.com>
2014-05-11 14:34:12 +0900

    updated the patch for oauth2 file

--- oauth2_patch.pl
+++ oauth2_patch.pl
@@ -4,25 +4,9 @@ use warnings;
 use File::Temp qw(tempfile);
 
 my $patch = <<'EOS';
---- /Users/yohei/node_modules/oauth/lib/oauth2.js	2012-07-28 21:46:01.000000000 +0900
-+++ node_modules/passport-oauth/node_modules/oauth/lib/oauth2.js	2013-02-17 16:16:56.000000000 +0900
-@@ -115,8 +115,8 @@
- 
- exports.OAuth2.prototype.getOAuthAccessToken= function(code, params, callback) {
-   var params= params || {};
--  params['client_id'] = this._clientId;
--  params['client_secret'] = this._clientSecret;
-+  //params['client_id'] = this._clientId;
-+  //params['client_secret'] = this._clientSecret;
-   params['type']= 'web_server';
-   var codeParam = (params.grant_type === 'refresh_token') ? 'refresh_token' : 'code';
-   params[codeParam]= code;
-@@ -124,6 +124,7 @@
-   var post_data= querystring.stringify( params );
-   var post_headers= {
-        'Content-Type': 'application/x-www-form-urlencoded'
-+     , 'Authorization': "Basic " + new Buffer(this._clientId + ":" + this._clientSecret).toString('base64')
-    };
+@@ -147,8 +147,8 @@
+
+
 EOS
 my ($fh, $patchfile) = tempfile();
 print $fh $patch;
